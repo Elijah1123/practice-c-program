b6367bf9a3f84057c1b122e2a478ff30a8aeac56
Elijah1123 <elijahmzalendo659@gmail.com>
2026-05-08 15:07:03 +0300

    final-inputs

--- main.c
+++ main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
     // Initializing variables
     int age = 0;
     float gpa = 0.0f;
     char grade = '\0';
-    char name[30] = "";
+    char name[50] = "";
 
     // 1. Get Age
     printf("Enter Your age: ");
@@ -21,15 +22,19 @@ int main() {
     scanf(" %c", &grade);
 
     // 4. Get Name
+    getchar();
     printf("Enter your name: ");
    
-    scanf("%s", name);
+    fgets(name, sizeof(name), stdin);
 
-    printf("\n--- Student Details ---\n");
+    name[strlen(name) -1] = '\0';
+
+    // printf("\n--- Student Details ---\n");
+    printf("Name:  %s\n", name);
     printf("Age:   %d\n", age);
     printf("GPA:   %.2f\n", gpa); // %.2f limits output to 2 decimal places
     printf("Grade: %c\n", grade);
-    printf("Name:  %s\n", name);
+    
 
     return 0;
 }
